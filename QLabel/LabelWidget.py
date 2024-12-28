7d0045400c492d1489b1bb0f3c478a2da7fcacf2
DatTran262 <trandat262075@gmail.com>
2024-12-29 01:48:49 +0700

    update login widget to btn facebook

--- QLabel/LabelWidget.py
+++ QLabel/LabelWidget.py
@@ -1,21 +1,24 @@
 from PyQt6.QtCore import Qt, QRect
-from PyQt6.QtWidgets import QLabel, QWidget, QApplication, QLineEdit
-from PyQt6.QtGui import QPixmap, QFont
+from PyQt6.QtWidgets import QLabel, QWidget, QApplication, QLineEdit, QGraphicsDropShadowEffect, QPushButton
+from PyQt6.QtGui import QPixmap, QFont, QColor, QIcon
 import sys
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setGeometry(1100, 200, 450, 550)
+        self.setGeometry(1500, 200, 450, 550) #laptop 1100, PC 1500
         self.setWindowTitle("LogIn")
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        self.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)
 
         # Label1
         label1 = QLabel(self)
         label1.setGeometry(30, 30, 370, 480)
-        label1.setStyleSheet("border-image: url(D:/PyQt/SnowTree.jpg);\n"
+        label1.setStyleSheet("border-image: url(D:/Code/PyCharm/Images/background.jpg);\n"
                              "border-radius: 20px;")
         label1.setScaledContents(True)
+        label1.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=25, xOffset=0, yOffset=0, color=QColor(234, 221, 186, 100)))
 
         # Label2
         label2 = QLabel(self)
@@ -25,17 +28,19 @@ class Window(QWidget):
 
         # Label3
         label3 = QLabel(self)
-        label3.setGeometry(50, 60, 330, 450)
+        label3.setGeometry(40, 60, 350, 450)
         label3.setStyleSheet("background-color: rgba(0, 0, 0, 75);\n"
                              "border-radius: 15px;")
+        label3.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=25, xOffset=0, yOffset=0, color=QColor(105, 221, 132, 100)))
 
         # Label4
         label4 = QLabel(self)
         label4.setGeometry(QRect(170, 90, 90, 45))
         font = QFont()
+        font.setFamily("Times New Roman")
         font.setPointSize(20)
         font.setBold(True)
-        font.setWeight(200)
+        font.setWeight(1000)
         label4.setFont(font)
         label4.setStyleSheet("color: rgba(255, 255, 255, 210);")
         label4.setText("Log In")
@@ -64,7 +69,50 @@ class Window(QWidget):
                                "border-bottom: 2px solid rgba(155, 168, 182, 255);\n"
                                "color: rgba(255, 255, 255, 255);\n"
                                "padding-bottom: 7px;")
+        lineEdit_Pass.setEchoMode(QLineEdit.EchoMode.Password)
         lineEdit_Pass.setPlaceholderText(" Password")
+
+        # Button Login
+        buttonLogIn = QPushButton(self)
+        buttonLogIn.setGeometry(QRect(115, 340, 200, 45))
+        buttonLogIn.setObjectName("buttonLogin")  # Gán objectName phù hợp
+        buttonLogIn.setStyleSheet("""
+            QPushButton#buttonLogin {
+                background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0.477, 
+                                                  stop:0 rgba(20, 47, 78, 219), stop:1 rgba(85, 98, 112, 226));
+                color: rgba(255, 255, 255, 210);
+                border-radius: 5px;
+            }
+            QPushButton#buttonLogin:hover {
+                background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0.477, 
+                                                  stop:0 rgba(40, 67, 98, 219), stop:1 rgba(105, 118, 132, 226));
+            }
+            QPushButton#buttonLogin:pressed {
+                background-color: rgba(105, 118, 132, 200);
+                padding-left: 5px;
+                padding-top: 5px;
+            }
+        """)
+        buttonLogIn.setText("L o g I n")
+        font = QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(15)
+        font.setBold(True)
+        font.setWeight(700)
+        buttonLogIn.setFont(font)
+
+        # Label forgot
+        label5 = QLabel(self)
+        label5.setGeometry(QRect(115, 375, 200, 45))
+        label5.setText("Forgot your User Name or Password?")
+        label5.setStyleSheet("color: rgba(255, 255, 255, 140);")
+        label5.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Button Facebook
+        btnFace = QPushButton(self)
+        btnFace.setGeometry(QRect(115, 435, 40, 40))
+        btnFace.setIcon(QIcon("D:/Code/PyCharm/Images/icon-facebook.svg"))
+
 
 app = QApplication(sys.argv)
 window = Window()
